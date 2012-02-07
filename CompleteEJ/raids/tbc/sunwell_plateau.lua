c69023b08b4970d21be92f55c74510d6eee5d756
wendell <wendell@gmail.com>
2012-02-07 11:40:35 +0800

    finish Sunwell

--- CompleteEJ/raids/tbc/sunwell_plateau.lua
+++ CompleteEJ/raids/tbc/sunwell_plateau.lua
@@ -22,14 +22,14 @@ local ENRAGE = 11
 local ENRAGE2 = 12
 
 local SUNWELL_PLATEAU = CreateInstance({
-  name = "Sunwell Plateau",
-  desc = "Sunwell Plateau is a 25-man raid instance located on the Isle of Quel'Danas which was implemented with Patch 2.4. The instance is intended to be the most difficult in the game until Wrath of the Lich King, and caters to Black Temple/Hyjal geared raiders. The instance bosses drop tokens redeemable for new pieces added to the Tier 6 Armor Sets and loot better than that from Black Temple. This raid dungeon requires no attunement.",
+  name = "太阳之井高地",
+  desc = "太阳之井高地是《魔兽世界》燃烧的远征中的25人顶级副本,太阳之井的新主人凯尔萨斯·逐日者依靠太阳之井的力量,召唤出了基尔加丹.玩家需要直接面对基尔加丹扭曲的化身.太阳之井能量的化身－安薇娜则被关在魔法屏障里.他们正在抽取她的能量以维持这个召唤仪式",
   --texture = "SunwellPlateau",
   area = 789,
 }, {
   {
-    name = "Kalecgos",
-    desc = "Kalecgos, one of the few surviving blue dragons, is the first boss in the Sunwell Plateau. He is being corrupted by Sathrovarr the Corruptor, a servant of Kil'jaeden.",
+    name = "卡雷苟斯",
+    desc = "卡雷苟斯是蓝龙军团的一名成员，作为少数在死亡之翼的毁灭中幸存下来的龙蛋中诞生的新一代巨龙，他强大、睿智，并且很快能够独当一面。在太阳之井被凯尔萨斯炸毁以后，心忧太阳之井能量下落的玛里苟斯派遣卡雷苟斯来到奎尔萨拉斯，任务是调查不久前被天灾毁灭的太阳之井的残余能量",
     --icon = true,
     creature = 24850,
     display = 23345,
@@ -50,8 +50,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "Sathrovarr the Corruptor",
-    desc = "Sathrovarr the Corruptor is a dreadlord tasked by Kil'jaeden with the corruption of the blue dragon Kalecgos. He is part of the first boss encounter of the Sunwell Plateau. Players encounter him in the inner veil, a shadowy spectral realm where they will find Kalecgos' humanoid form battling the demon to stave off the corruption.",
+    name = "腐蚀者萨索瓦尔",
+    desc = "萨索瓦尔是一名恐惧魔王,被燃烧军团派遣去腐蚀卡雷苟斯的内心.玩家在面对太阳井1号BOSS时,将会在恶魔领域的位面中与这名恐惧魔王交手,击败他即可让卡雷苟斯重获自由.",
     --icon = true,
     creature = 24892,
     display = 26628,
@@ -65,8 +65,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "Brutallus",
-    desc = "Brutallus is a pit lord and the second boss of the Sunwell Plateau. He can also be seen from outside the instance, where he is engaging the blue dragon Madrigosa. He succeeds in slaying the dragon and turns her into Felmyst.",
+    name = "布鲁塔布斯",
+    desc = "布鲁塔布斯是一名强大的深渊领主,盘踞在太阳井高地的死亡之痕处.他杀死了蓝龙玛蒂苟萨并且将她变成了菲米丝.",
     --icon = true,
     creature = 24882,
     display = 22711,
@@ -80,8 +80,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "Felmyst",
-    desc = "Felmyst is a fel dragon who is raised from from the corpse of the blue dragon Madrigosa. She is the third boss in the Sunwell Plateau, and can only be fought once Brutallus has been defeated.",
+    name = "菲米丝",
+    desc = "布鲁塔布斯的深渊恶魔之力腐蚀了玛蒂苟萨,她死后的尸体变成了菲米丝.如果玩家想要通过死亡之痕,那么就要击败菲米丝,让玛蒂苟萨的灵魂得到超度",
     --icon = true,
     creature = 25038,
     display = 22838,
@@ -96,8 +96,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "Grand Warlock Alythess",
-    desc = "Grand Warlock Alythess, a man'ari eredar, is the fire based demon eredar twin located in Sunwell Plateau.",
+    name = "高阶术士奥蕾塞丝",
+    desc = "艾瑞达双子是基尔加丹的两位女性副官，高阶术士奥蕾塞丝和萨洛拉丝女王.高阶术士奥蕾塞丝，艾瑞达恶魔，双子姐妹中的火焰施法者.",
     --icon = true,
     creature = 25166,
     display = 23334,
@@ -115,8 +115,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "Lady Sacrolash",
-    desc = "Lady Sacrolash, a man'ari eredar, is the shadow based demon eredar twin located in Sunwell Plateau.",
+    name = "萨洛拉丝女王",
+    desc = "艾瑞达双子是基尔加丹的两位女性副官，高阶术士奥蕾塞丝和萨洛拉丝女王.萨洛拉丝女王，艾瑞达恶魔，双子姐妹中的暗影施法者.",
     --icon = true,
     creature = 25165,
     display = 23177,
@@ -133,8 +133,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "M'uru",
-    desc = "M'uru is a fallen naaru and former guardian of Tempest Keep. After his defeat in Tempest Keep, Kael'thas and his felblood minions captured M'uru from Silvermoon and took him to the Sunwell Plateau, presumably to drain his power and aid in the summoning of Kil'jaeden. As such, he is now the fifth boss of the instance.",
+    name = "穆鲁",
+    desc = "穆鲁曾经是一名高贵的纳鲁,在被凯尔萨斯劫持到太阳之井并且圣光力量极度虚弱的情况下，被基尔加丹的力量腐蚀，堕落为黑暗纳鲁，并由于是纯能量体，还转化成为了可怕的熵魔.",
     --icon = true,
     creature = 25741,
     display = 23404,
@@ -146,8 +146,8 @@ local SUNWELL_PLATEAU = CreateInstance({
     },
   },
   {
-    name = "Kil'jaeden",
-    desc = "Kil'jaeden the Deceiver is a powerful eredar demon lord and is the acting leader of the Burning Legion. He is the sixth and final boss of the instance where [layers face off against this twisted avatar of Kil'jaeden, which is still being summoned into Azeroth through the Sunwell itself by his new lieutenant, Kael'thas Sunstrider. Anveena, the embodiment of the Sunwell's energies, is held in an arcane barrier, her powers being drained to feed the portal.",
+    name = "欺诈者基尔加丹",
+    desc = "基尔加丹来自恶魔种族艾瑞达，与阿克蒙德一起是最先加入燃烧军团的两位恶魔勇士。他俩听命于恶魔之王萨格拉斯，率领燃烧军团与泰坦世界为敌.当伊利丹盘踞于黑暗神殿时，他手下的一名悍将——凯尔萨斯，因为渴望比变节者更强大的力量，而选择了他以前的主人——基尔加丹作为他新的主人。凯尔萨斯率领着一支号称“拂晓之刃”的血精灵部队重新恢复了血精灵曾经依赖的东西——太阳之井，企图以此来召唤他新的主人——欺诈者基尔加丹来到艾泽拉斯，重现千年前的灾难和自己期盼已久,奎尔萨拉斯的荣耀。",
     --icon = true,
     creature = 25315,
     display = 23200,
