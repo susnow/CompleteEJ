f1f7ad1158d68945acabb8335c69910f328c6416
wendell <wendell@gmail.com>
2012-02-08 17:09:12 +0800

    finish utgarde_pinnacle

--- CompleteEJ/dungeons/wotlk/utgarde_pinnacle.lua
+++ CompleteEJ/dungeons/wotlk/utgarde_pinnacle.lua
@@ -1,4 +1,4 @@
-local _, db = ...
+﻿local _, db = ...
 local db, CreateInstance, Stage, Spell = db[1], db.CreateInstance, db.Stage, db.Spell
 
 local VANILLA, TBC, WOTLK = 1, 2, 3
@@ -22,14 +22,14 @@ local ENRAGE = 11
 local ENRAGE2 = 12
 
 local UTGARDE_PINNACLE = CreateInstance({
-  name = "Utgarde Pinnacle",
-  desc = "Utgarde Keep is the first dungeon hub located on the shores of Lake Cauldros in the Howling Fjord of Northrend. Inhabited by the vrykul (led by King Ymiron), a half-giant Viking-like race bent on proving their strength to the Lich King, they are raising the most worthy of their warriors to serve him at the main base of operations. Utgarde Pinnacle is the second and highest level 5 man wing located within Utgarde Keep.",
+  name = "乌特加德之巅",
+  desc = "乌特加德之巅是嚎风峡湾区域的副本之一，位于乌特加德城堡附近，是由巫妖王的手下维库人所掌控的区域，这是一个80级的副本，除了一般的维库人战士之外，维库人之王伊米隆国王也在这个副本的深处.",
   --texture = "UtgardePinnacle",
   area = 524,
 }, {
   {
-    name = "Svala Sorrowgrave",
-    desc = "Svala Sorrowgrave is the first boss in Utgarde Pinnacle. She is killed and afterwards turned into a Val'kyr. She is related to the achievement The Incredible Hulk.",
+    name = "席瓦拉·索格蕾",
+    desc = "席瓦拉·索格蕾在死后被巫妖王复活成为了一名瓦格雷.",
     --icon = true,
     creature = 26668,
     display = 26096,
@@ -46,8 +46,8 @@ local UTGARDE_PINNACLE = CreateInstance({
     },
   },
   {
-    name = "Gortok Palehoof",
-    desc = "Gortok Palehoof is the second boss in Utgarde Pinnacle. The giant magnataur will become active once the four minibosses in the preceding arena event have been defeated.",
+    name = "戈托克·苍蹄",
+    desc = "巨型猛犸兽,要挑战戈托克·苍蹄你需要激活并击败它之前的4个守护巨兽",
     --icon = true,
     creature = 26687,
     display = 27419,
@@ -62,8 +62,8 @@ local UTGARDE_PINNACLE = CreateInstance({
     },
   },
   {
-    name = "Skadi the Ruthless",
-    desc = "Skadi the Ruthless is the third boss in Utgarde Pinnacle. The vrykul and his mount, Grauf, will fight adventurers in a gauntlet event. Players enjoy returning to solo this encounter for Reins of the Blue Proto-Drake on heroic. Skadi is also related to the achievements My Girl Loves to Skadi All the Time and Lodi Dodi We Loves the Skadi.",
+    name = "残忍的斯卡迪",
+    desc = "玩家需要杀死场面上刷新的维库人来获取鱼叉,并且使用鱼叉发射器将斯卡迪从坐骑上射下来并且开始这场战斗,斯卡迪除了掉落蓝龙坐骑之外,还和许多成就相关.",
     --icon = true,
     creature = 26693,
     display = 27418,
@@ -79,8 +79,8 @@ local UTGARDE_PINNACLE = CreateInstance({
     },
   },
   {
-    name = "King Ymiron",
-    desc = "King Ymiron is the fourth and last boss in Utgarde Pinnacle. He is related to the achievement King's Bane.",
+    name = "伊米尔隆国王",
+    desc = "伊米尔隆国王,维库人之王,同时也是乌特加德之巅的最后守关Boss.",
     --icon = true,
     creature = 26861,
     display = 28019,
@@ -99,27 +99,27 @@ local UTGARDE_PINNACLE = CreateInstance({
       Spell(48423, {fbd = N5}), -- Spirit Strike
     },
   },
-  {
-    name = "King Ymiron",
-    desc = "King Ymiron is the fourth and last boss in Utgarde Pinnacle. He is related to the achievement King's Bane.",
+  -- {
+    -- name = "King Ymiron",
+    -- desc = "King Ymiron is the fourth and last boss in Utgarde Pinnacle. He is related to the achievement King's Bane.",
     --icon = true,
-    creature = 26861,
-    display = 28019,
-    coords = {0.406, 0.534, 2},
-    loot = {{37401,H5},{37065,N5},{37408,H5},{37390,H5},{37062,N5},{37397,H5},{37395,H5},{37407,H5},{37067,N5},{43102,H5},{37398,H5},41797,{37060,N5},{37409,H5},{37066,N5},{37064,N5},{37061,N5},{37058,N5},},
-    sections = {
-      Spell(59301, {fbd = H5, flag1 = MAGIC}), -- Bane
-      Spell(48294, {flag1 = MAGIC}), -- Bane
-      Spell(48292), -- Dark Slash
-      Spell(59300, {fbd = H5, flag1 = DISEASE}), -- Fetid Rot
-      Spell(48291, {flag1 = DISEASE}), -- Fetid Rot
-      Spell(51750), -- Screams of the Dead
-      Spell(48529, {fbd = N5}), -- Spirit Burst
-      Spell(59305, {fbd = H5}), -- Spirit Burst
-      Spell(59304, {fbd = H5}), -- Spirit Strike
-      Spell(48423, {fbd = N5}), -- Spirit Strike
-    },
-  },
+    -- creature = 26861,
+    -- display = 28019,
+    -- coords = {0.406, 0.534, 2},
+    -- loot = {{37401,H5},{37065,N5},{37408,H5},{37390,H5},{37062,N5},{37397,H5},{37395,H5},{37407,H5},{37067,N5},{43102,H5},{37398,H5},41797,{37060,N5},{37409,H5},{37066,N5},{37064,N5},{37061,N5},{37058,N5},},
+    -- sections = {
+      -- Spell(59301, {fbd = H5, flag1 = MAGIC}), -- Bane
+      -- Spell(48294, {flag1 = MAGIC}), -- Bane
+      -- Spell(48292), -- Dark Slash
+      -- Spell(59300, {fbd = H5, flag1 = DISEASE}), -- Fetid Rot
+      -- Spell(48291, {flag1 = DISEASE}), -- Fetid Rot
+      -- Spell(51750), -- Screams of the Dead
+      -- Spell(48529, {fbd = N5}), -- Spirit Burst
+      -- Spell(59305, {fbd = H5}), -- Spirit Burst
+      -- Spell(59304, {fbd = H5}), -- Spirit Strike
+      -- Spell(48423, {fbd = N5}), -- Spirit Strike
+    -- },
+  -- },
 })
 
 table.insert(db, UTGARDE_PINNACLE)
